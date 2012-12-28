222a46f6f9506685e88b02a08092857a893372ef
Stephen LePera <stepehen.lepera@gmail.com>
2012-12-28 01:00:14 -0500

    3rd party intersection routine added for projection

---
+++ multi_angle_mie_sizing_3rd_party/helper_functions/plane_line_intersect.m
@@ -0,0 +1,95 @@
+function [I,check]=plane_line_intersect(n,V0,P0,P1)
+%plane_line_intersect computes the intersection of a plane and a segment(or
+%a straight line)
+% Inputs: 
+%       n: normal vector of the Plane 
+%       V0: any point that belongs to the Plane 
+%       P0: end point 1 of the segment P0P1
+%       P1:  end point 2 of the segment P0P1
+%
+%Outputs:
+%      I    is the point of interection 
+%     Check is an indicator:
+%      0 => disjoint (no intersection)
+%      1 => the plane intersects P0P1 in the unique point I
+%      2 => the segment lies in the plane
+%      3=>the intersection lies outside the segment P0P1
+%
+% Example:
+% Determine the intersection of following the plane x+y+z+3=0 with the segment P0P1:
+% The plane is represented by the normal vector n=[1 1 1]
+% and an arbitrary point that lies on the plane, ex: V0=[1 1 -5]
+% The segment is represented by the following two points
+% P0=[-5 1 -1]
+%P1=[1 2 3]   
+% [I,check]=plane_line_intersect([1 1 1],[1 1 -5],[-5 1 -1],[1 2 3]);
+
+%This function is written by :
+%                             Nassim Khaled
+%                             Wayne State University
+%                             Research Assistant and Phd candidate
+%If you have any comments or face any problems, please feel free to leave
+%your comments and i will try to reply to you as fast as possible.
+
+I=[0 0 0];
+u = P1-P0;
+w = P0 - V0;
+D = dot(n,u);
+N = -dot(n,w);
+check=0;
+if abs(D) < 10^-7        % The segment is parallel to plane
+        if N == 0           % The segment lies in plane
+            check=2;
+            return
+        else
+            check=0;       %no intersection
+            return
+        end
+end
+
+%compute the intersection parameter
+sI = N / D;
+I = P0+ sI.*u;
+
+if (sI < 0 || sI > 1)
+    check= 3;          %The intersection point  lies outside the segment, so there is no intersection
+else
+    check=1;
+end
+
+% 
+% Copyright (c) 2008, Nassim Khaled
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are 
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright 
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in 
+%       the documentation and/or other materials provided with the distribution
+%       
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
+
+
+
+
+
+
+
+
+
+
+
